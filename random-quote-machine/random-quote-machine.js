cf6e13c9dfc8a89f910dfd3ceb412ea90940a5b3
Fiquinho <ficobogado@gmail.com>
2017-08-02 22:04:36 -0300

    fixed quote API

--- random-quote-machine/random-quote-machine.js
+++ random-quote-machine/random-quote-machine.js
@@ -1,14 +1,9 @@
 $(document).ready(function() {
     console.info("hi");
-    $.ajaxSetup({
-        headers : {
-            "Access-Control-Allow-Origin": "*"
-        }
-    });
 
-    $.getJSON("http://api.forismatic.com/api/1.0/method=getQuote&format=json&lang=en", function(json) {
+
+    $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(json) {
          $(".quote").html(JSON.stringify(json));
        });
-
 
 });
