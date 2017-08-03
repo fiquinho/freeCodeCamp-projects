8638888a6cf18136ecd666f8578d0f87438b821d
Fiquinho <ficobogado@gmail.com>
2017-08-02 23:19:37 -0300

    finished style and added next quote button

--- random-quote-machine/random-quote-machine.js
+++ random-quote-machine/random-quote-machine.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
     
     displayQuote();
 
+    $(".button").click(displayQuote);
+
 });
 
 function displayQuote() {
+    
+    // Documentation: http://quotes.stormconsultancy.co.uk/api
     $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(json) {
 
         var quote = jQuery.parseJSON( JSON.stringify(json) );
