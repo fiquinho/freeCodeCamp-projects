86f52affb7f0edf6f8e0acb3051a849199ad49c6
Fiquinho <ficobogado@gmail.com>
2017-08-05 00:33:16 -0300

    added twitter functionallity

--- random-quote-machine/random-quote-machine.js
+++ random-quote-machine/random-quote-machine.js
@@ -4,6 +4,24 @@ $(document).ready(function() {
 
     $(".button").click(displayQuote);
 
+    $('.twitter').click(function() {
+        var url = 'https://twitter.com/intent/tweet/?text=';
+        var quote = $(".quote").html();
+        var author = $(".author").html();
+
+        var query = quote + " - " + author + "&via=FedBogado"
+
+        if(query.length <= 140) {
+            url += quote + " - " + author + "&via=FedBogado" ;
+            window.location.href = url;
+        } else {
+            $(".alert").css("display", "initial");
+        }
+
+
+        return false;
+    });
+
 });
 
 function displayQuote() {
@@ -16,5 +34,6 @@ function displayQuote() {
         $(".quote").html(quote.quote);
         $(".author").html(quote.author);
 
+        $(".alert").css("display", "none");
     });
 };
